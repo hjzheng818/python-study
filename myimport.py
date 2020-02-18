7fb62fe2468208b30fdf766e2e7e092a04e89636
hjzheng818 <hjzheng818@126.com>
2020-02-18 17:25:08 +0800

    <Python For Data Analysis> study ch02
    jupyter file upload for other study's friends.

---
+++ myimport.py
@@ -0,0 +1,11 @@
+import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import Series,DataFrame
+import tushare as ts
+import base64 
+import pytesseract as pt
+import pandas.io as web
+
+print 'import Image numpy->np matplotlib->plt pandas->pd tushare->ts base64 pytesseract->pt success!'
